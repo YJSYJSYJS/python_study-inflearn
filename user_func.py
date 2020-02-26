7b97f23e4f42f033bc2f310732f7918fd60a1d68
WhyJS <dbs106@gmail.com>
2020-02-27 00:49:52 +0900

    user function - 1

---
+++ user_func.py
@@ -0,0 +1,30 @@
+# 사용자 함수
+
+def 함수명():
+    print("함수호출")
+    return True
+
+a = 함수명()
+
+def add(a,b):
+    return a+b
+
+c = 10
+def add2(a,b):
+    c = a+b
+    # 지역변수의 개념
+    return c
+
+
+b = add(1,10)
+print(b, c)
+
+d = 20
+def add3(a,b):
+    global d
+    d = a+b
+    # 지역변수의 개념
+    return d
+    
+r = add3(1,1)
+print(r,d)
